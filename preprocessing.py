c48bd78c7e5baa9975c5eb4bb0fb0edc4853c7e4
Eva <e.planje@student.tudelft.nl>
2026-03-09 15:33:36 +0100

    Commit: verberteren preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -5,10 +5,6 @@ import os
 import numpy as np
 from load_data import load_data, split_pd
 
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.expand_frame_repr', False)
-
 
 ##% Oproepen data en testset weer verkrijgen
 
