f7f17d91be7494bf2d7eb2015043494713cead34
Eva <e.planje@student.tudelft.nl>
2026-03-17 11:06:52 +0100

    fs_mutualinformation klaar

---
+++ fs_mutualinformation.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import os
+import numpy as np
+from load_data import load_data, split_pd, explore_data, plot_feature_pairs, plot_heatmap
+from preprocessing import apply_normalization, remove_zero_variance_features
+from sklearn.feature_selection import mutual_info_classif
+
+#%% Mutual information
+
+def fs_mutualinformation(df, labels, k, showdetails=True):
+    mi_scores = mutual_info_classif(
+        df, 
+        labels, 
+        discrete_features=False
+    )
+    
+    mi_df = pd.DataFrame({
+        'feature': df.columns,
+        'mi_score': mi_scores
+    }).sort_values(by='mi_score', ascending=False).reset_index(drop=True)
+
+    selected_features_mi = mi_df['feature'].head(k).tolist()
+
+    result_dict_mi = {
+        "method": "mutual_information",
+        "scores": mi_df,
+        "selected_features": selected_features_mi,
+        "n_selected": len(selected_features_mi)
+    }
+    if showdetails:
+        print(result_dict_mi["scores"].head(k).to_string(index=False))
+
+    return result_dict_mi
+
+
+#%% Inladen data
+GIST_data = load_data('GIST_radiomicFeatures.csv')
+GIST_train, GIST_test, y_train, y_test = split_pd(GIST_data, False)
+normalized_GIST_train, scaler = apply_normalization(GIST_train)
+preproc_GIST_train, kept_features = remove_zero_variance_features(normalized_GIST_train, show_details=False)
+
+#%% Printen van de resultaten
+
+mi_results = fs_mutualinformation(preproc_GIST_train, y_train, 20, False)
