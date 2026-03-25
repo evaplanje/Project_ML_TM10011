f1a21fc836df808b9261f670b0b3582ea3eee439
Eva <e.planje@student.tudelft.nl>
2026-03-25 20:01:15 +0100

    Toevoegen AUC score

--- Wilxocon_test.py
+++ Wilxocon_test.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 from scipy.stats import wilcoxon
 from itertools import combinations
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 #%% ---------------- LAAD ALLE PICKLE BESTANDEN ----------------
 with open('model_scores_RF.pkl', 'rb') as f:
@@ -63,9 +65,48 @@ print(results_df.to_string(index=False))
 
 # ---------------- WINNERS TELLEN ----------------
 print("\n=== Aantal overwinningen per model ===")
-for model, count in win_counts.items():
+
+sorted_win_counts = sorted(win_counts.items(), key=lambda x: x[1], reverse=True)
+
+for model, count in sorted_win_counts:
     print(f"{model}: {count} overwinningen")
 
+#%% ---------------- PLOTTEN VAN DE AUC SCORES -----------------
 
+# Maak een nieuwe dictionary met gemiddelde AUC voor elk model
+mean_auc_scores = {model: np.mean(scores) for model, scores in all_model_scores.items()}
+std_auc_scores = {model: np.std(scores) for model, scores in all_model_scores.items()}
 
+# Matrix maken voor de heatmap
+feature_selection_methods = ['LASSO', 'MRMR', 'MI', 'RFE']
+classifiers = ['RF', 'SVM', 'XGB']
+auc_matrix = pd.DataFrame(index=feature_selection_methods, columns=classifiers)
+std_matrix = pd.DataFrame(index=feature_selection_methods, columns=classifiers)
+
+# Vul de matrix met de gemiddelde AUC-scores
+for fs_method in feature_selection_methods:
+    for clf in classifiers:
+        model_key = f'{fs_method}_{clf}' 
+        auc_matrix.loc[fs_method, clf] = mean_auc_scores[model_key] 
+        std_matrix.loc[fs_method, clf] = std_auc_scores[model_key]
+
+auc_matrix = auc_matrix.apply(pd.to_numeric, errors='coerce') # nummerieke waardes maken 
+std_matrix = std_matrix.apply(pd.to_numeric, errors='coerce')
+
+# Maak de heatmap
+plt.figure(figsize=(10, 6))
+sns.heatmap(auc_matrix, annot=True, cmap='Blues', cbar=True, fmt='.3f', linewidths=0.5)
+# als je wel die std erbij wil, dan dit nog toevoegen annot_kws={'color': 'black'}
+
+# Toevoegen std
+# for (i, j), value in np.ndenumerate(auc_matrix.values):
+#     std_value = std_matrix.iloc[i, j]
+#     plt.text(j + 0.75, i + 0.5, f'±{std_value:.3f}', 
+#              ha='center', va='center', color='black', fontsize=10)
+    
+
+plt.title('Mean AUC scores and std for each feature selection method and classifier')
+plt.ylabel('Feature Selection Methoden')
+plt.xlabel('Classifiers')
+plt.show()
 #%% ------
